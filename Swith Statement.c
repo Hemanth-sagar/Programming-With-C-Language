034cff2e4715e6e61a2f7dba6c1605f03043be43
Hemanth-sagar <71266236+Hemanth-sagar@users.noreply.github.com>
2021-08-19 16:18:15 +0530

    switch to represent Numbers

---
+++ Swith Statement.c
@@ -0,0 +1,31 @@
+//Demonstrating Switch Expression
+int main()
+{
+ //Switch(expression)
+ int number;
+ printf("enter number to represent");
+ scanf("%d",&number);
+ switch(number)
+ {
+    case 1:
+        printf("one");
+    case 2:
+        printf("two");
+    case 3:
+        printf("two");
+    case 4:
+        printf("four");
+    case 5:
+        printf("five");
+    case 6:
+        printf("six");
+    case 7:
+        printf("seven");
+    case 8:
+        printf("eight");
+    case 9:
+        printf("nine");
+    default:
+        printf("Enter Between 1-9");
+ }
+}
